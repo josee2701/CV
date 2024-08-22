18b4b51a24d88f9e6994261fe6ef2b416ee8d51d
Jose Campos <j.camposs2701@gmail.com>
2024-08-21 23:41:34 -0500

    feat: Se realiza modificacion al componente de resumen para que la informacion se visualice correctamente

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // Importar BrowserRouter
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import { initDB } from 'react-indexed-db-hook';
-import { DBConfig } from './indexedDBConfig';
-
-initDB(DBConfig);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> {/* Envolver App con BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-if ('serviceWorker' in navigator && 'SyncManager' in window) {
-  window.addEventListener('load', () => {
-      navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/serviceWorker.js`).then((registration) => {
-          console.log('Registro de ServiceWorker exitoso con el alcance: ', registration.scope);
-      }, (error) => {
-          console.log('Registro de ServiceWorker fallido: ', error);
-      });
-  });
-}
 
 // Si quieres empezar a medir el rendimiento en tu aplicación, pasa una función
 // para registrar los resultados (por ejemplo: reportWebVitals(console.log))
